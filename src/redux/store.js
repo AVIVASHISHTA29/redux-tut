127ea809170620dff28e372f2f3238eb8cf8ebf4
AviVashishta29 <avivashishta@gmail.com>
2024-12-13 21:18:16 +0530

    redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 // store.js
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
